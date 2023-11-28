ab8ea27609b21bf29c7452697d9c82eeb5fdbb9b
Suez_kip <287140262@qq.com>
2023-11-28 20:26:46 +0800

    2023_11_28_20_26_46

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
 set https_proxy=127.0.0.1:7890
 git add *
-git commit -m %DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%__%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
+git commit -m %DATE:~0,4%_%DATE:~5,2%_%DATE:~8,2%_%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
 git pull
 git push
